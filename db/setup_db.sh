695fac707b9dc6bca91a26f799bedd8c2025c81a
Natalia Holkova <xholko02@stud.fit.vutbr.cz>
2019-10-16 22:44:09 +0200

    Auto insert initial values during setup + typo fix in db_schema

--- db/setup_db.sh
+++ db/setup_db.sh
@@ -3,4 +3,5 @@ DB_NAME=iis_db
 sudo -u postgres psql -c "DROP DATABASE IF EXISTS $DB_NAME;"
 sudo -u postgres psql --echo-all -c "CREATE DATABASE $DB_NAME;"
 sudo -u postgres psql --echo-all -d $DB_NAME -f ./create_tables.sql
+sudo -u postgres psql --echo-all -d $DB_NAME -f ./insert_values.sql
 exit
