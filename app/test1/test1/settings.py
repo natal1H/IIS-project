8a280be0a9e5279b046ec5df3c4bd82efb8c6d98
Natalia Holkova <xholko02@stud.fit.vutbr.cz>
2019-10-25 16:40:47 +0200

    Changed db to Postgres

--- app/test1/test1/settings.py
+++ app/test1/test1/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'test1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'canteen',
+        'USER': 'admin',
+        'PASSWORD': 'adminadmin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
