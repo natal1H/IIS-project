2ca5dcaaa802e1ec822a185231d6ee4586e50722
Natalia Holkova <xholko02@stud.fit.vutbr.cz>
2019-12-20 20:48:19 +0100

    Layout of admin/operator/driver links

--- app/app/settings.py
+++ app/app/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'canteen.context_processors.add_person_role_to_context',
             ],
         },
     },
